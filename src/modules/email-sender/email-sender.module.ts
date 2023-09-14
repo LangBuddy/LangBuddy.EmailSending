d1560209f72e83f71d8a46b8be7598da51b8f379
FremyXS <antonovch13@mail.ru>
2023-09-14 14:55:16 +0500

    feat(modules/email-sender): add import MailerModule

--- src/modules/email-sender/email-sender.module.ts
+++ src/modules/email-sender/email-sender.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { EmailSenderService } from './email-sender.service';
 import { EmailSenderController } from './email-sender.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [EmailSenderController],
   providers: [EmailSenderService],
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('MAIL_HOST'),
+          secure: true,
+          auth: {
+            user: config.get('MAIL_USER'),
+            pass: config.get('MAIL_PASSWORD'),
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class EmailSenderModule {}
