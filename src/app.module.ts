cabce42884623033876f38a8aee21d91f583ab56
FremyXS <antonovch13@mail.ru>
2023-09-14 14:55:50 +0500

    feat: update app modue imports
    add ConfigModule, EmailSenderModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EmailSenderModule } from './modules/email-sender/email-sender.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EmailSenderModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    EmailSenderModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
