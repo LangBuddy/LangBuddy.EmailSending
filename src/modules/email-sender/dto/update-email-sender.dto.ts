d976e8bf6aac98db64026d6e58b00eba023ef863
FremyXS <antonovch13@mail.ru>
2023-09-14 12:15:07 +0300

    refactor(modules): remove other files

--- src/modules/email-sender/dto/update-email-sender.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEmailSenderDto } from './create-email-sender.dto';
-
-export class UpdateEmailSenderDto extends PartialType(CreateEmailSenderDto) {}
