b4664a711e3c4113f159966c149d42db98a85e20
FremyXS <antonovch13@mail.ru>
2023-09-14 14:54:35 +0500

    feat(modules/email-sender): add endpoint to email send

--- src/modules/email-sender/email-sender.controller.ts
+++ src/modules/email-sender/email-sender.controller.ts
@@ -1,45 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { EmailSenderService } from './email-sender.service';
-import { CreateEmailSenderDto } from './dto/create-email-sender.dto';
-import { UpdateEmailSenderDto } from './dto/update-email-sender.dto';
+import { EmailBodyRequest } from './request/email-body.request';
 
 @Controller('email-sender')
 export class EmailSenderController {
-  constructor(private readonly emailSenderService: EmailSenderService) {}
+  constructor(private readonly mailService: EmailSenderService) {}
 
   @Post()
-  create(@Body() createEmailSenderDto: CreateEmailSenderDto) {
-    return this.emailSenderService.create(createEmailSenderDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.emailSenderService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.emailSenderService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateEmailSenderDto: UpdateEmailSenderDto,
-  ) {
-    return this.emailSenderService.update(+id, updateEmailSenderDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.emailSenderService.remove(+id);
+  async sendEmail(@Body() emailBodyRequest: EmailBodyRequest) {
+    return this.mailService.sendEmail(emailBodyRequest);
   }
 }
